ee24f10f9f0990e62707fd0226b2cf68428adfd4
smirnov <m.smirnov@greenlabs.ru>
2014-10-10 15:25:47 +0400

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-antivirus-field',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/budurli/django-antivirus-field',
+    license='MIT',
+    author='Maxim Smirnoff',
+    author_email='smirnoffmg@gmail.com',
+    description='Django FileField with clamav protection'
+)
