913550e883a03020e8302bd9c2a7a89c700fb503
Kai Nessig <gitlab@dasgibtsdochso.net>
2015-11-12 17:45:50 +0100

    raise ValidationError depending on configuration setting when virus scanning itself fails

--- django_antivirus_field/validators.py
+++ django_antivirus_field/validators.py
@@ -6,9 +6,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from django_antivirus_field.utils import is_infected
 
+from django.conf import settings
+CLAMAV_FAIL_CASCADE = getattr(settings, 'CLAMAV_FAIL_CASCADE', False)
+
 
 def file_validator(f):
     has_virus, name = is_infected(f.file.read())
-    if has_virus:
-        raise ValidationError(_('Virus "{}" was detected').format(name))
 
+    if has_virus is None and CLAMAV_FAIL_CASCADE:
+        raise ValidationError(_('Virus checking failed'))
+
+    if has_virus:
+        raise ValidationError(_('Virus "%(name)s" was detected'), params={'name': name})
